c13513cb1f2ed37caeaa542b8cf611296889cfd9
Euan-Benson <euanbson@gmail.com>
2026-03-12 12:06:27 +0000

    add stop function

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ function myFunction(e) {
   let y = e.clientY;
   let coor = "Coordinates: (" + x + "," + y + ")";
   document.getElementById("demo").innerHTML = coor;
-  document.getElementById("image").src = "moutains.jpg";
+  //   document.getElementById("image").src = "moutains.jpg";
 }
 
 $(document).ready(function () {
   $(document).mousemove(function (e) {
     let x = e.clientX;
     let y = e.clientY;
-    $("img").css("top", y + "px");
-    $("img").css("left", x + "px");
+    // $("img").css("top", y + "px");
+    // $("img").css("left", x + "px");
   });
 });
 
@@ -19,28 +19,35 @@ let x = 0,
   y = 0,
   dirX = 1,
   dirY = 1;
-const speed = 2;
-let image = document.getElementById("popup");
-const imageWidth = image.clientWidth;
-const imageHeight = image.clientHeight;
+let speed = 2;
+let popup = document.getElementById("popup");
+const popupWidth = popup.clientWidth;
+const popupHeight = popup.clientHeight;
 
 function animate() {
   const screenHeight = document.body.clientHeight;
   const screenWidth = document.body.clientWidth;
 
-  if (y + imageHeight >= screenHeight || y < 0) {
+  if (y + popupHeight >= screenHeight || y < 0) {
     dirY *= -1;
   }
-  if (x + imageWidth >= screenWidth || x < 0) {
+  if (x + popupWidth >= screenWidth || x < 0) {
     dirX *= -1;
   }
   x += dirX * speed;
   y += dirY * speed;
-  image.style.left = x + "px";
-  image.style.top = y + "px";
+  popup.style.left = x + "px";
+  popup.style.top = y + "px";
   window.requestAnimationFrame(animate);
-  document.getElementById("image").src = "";
+  $("body").css("cursor", "none");
+  //   document.getElementById("image").src = "";
 }
 
-setTimeout(animate,10000);
+function stopAnimation(e) {
+  speed = 0;
+  //document.getElementById("image").src = "moutains.jpg";
+  $("body").css("cursor", "default");
+  //   console.log(document.getElementById("image").src);
+}
 
+setTimeout(animate, 5000);
