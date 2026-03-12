3de50127c6c01dfecccf4018679dbebc9e944069
Euan-Benson <euanbson@gmail.com>
2026-03-12 10:44:15 +0000

    add bouncing image

--- script.js
+++ script.js
@@ -6,20 +6,42 @@ function myFunction(e) {
   document.getElementById("image").src = "moutains.jpg";
 }
 
-function drift() {
-  let nX = e.clientX + 100;
-  let nY = e.clientY + 100;
-  $("img").css("top", nY + "px");
-  $("img").css("left", nX + "px");
-}
-
 $(document).ready(function () {
   $(document).mousemove(function (e) {
     let x = e.clientX;
     let y = e.clientY;
     $("img").css("top", y + "px");
     $("img").css("left", x + "px");
-    window.setInterval(drift,5000);
   });
 });
+
+let x = 0,
+  y = 0,
+  dirX = 1,
+  dirY = 1;
+const speed = 2;
+let dvd = document.getElementById("image");
+const dvdWidth = dvd.clientWidth;
+const dvdHeight = dvd.clientHeight;
+
+function animate() {
+  const screenHeight = document.body.clientHeight;
+  const screenWidth = document.body.clientWidth;
+
+  if (y + dvdHeight >= screenHeight || y < 0) {
+    dirY *= -1;
+  }
+  if (x + dvdWidth >= screenWidth || x < 0) {
+    dirX *= -1;
+  }
+  x += dirX * speed;
+  y += dirY * speed;
+  dvd.style.left = x + "px";
+  dvd.style.top = y + "px";
+  window.requestAnimationFrame(animate);
+}
+
+setTimeout(animate,10000);
+
+dvd.addEventListener("click", animate);
 
